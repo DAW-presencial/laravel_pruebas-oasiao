1bfae8000a404c5bf7e75cfb1a2a67008e603425
oasiao <74007996+oasiao@users.noreply.github.com>
2022-02-18 13:34:38 +0100

    routes added for api and resource

--- pruebasApi/routes/web.php
+++ pruebasApi/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
 });
+
+//Route::get('/api',ApiContactsController::class);
+//Route::get('/myContacts',[ApiContactsController::class,'']);
+
+Route::resource('/contacts', ContactsController::class);
